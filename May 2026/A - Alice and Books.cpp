bae734bb8c1ba587fcfeb0a17b0dc2a3bc47db64
Brandom Galder Hernandez Franco <galderhernandez571@gmail.com>
2026-05-28 23:24:49 -0600

    Day 4

---
+++ May 2026/A - Alice and Books.cpp
@@ -0,0 +1,45 @@
+//URL: https://codeforces.com/problemset/problem/1978/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int MAXN = 200001;
+const int MOD = 1e9 + 7;
+#define pb push_back
+
+#define all(c) (c).begin(), (c).end()
+#define find_set(c, a) ((c).find(a) != (c).end())
+#define in_range(x, l, r) ((x) >= (l) && (x) <= (r))
+#define debug(x) cout << #x << " -> " << x <<'\n';
+
+int arr[101];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while (t--) {
+        int n;
+        cin>>n;
+
+        for (int i=1; i<=n; i++) cin>>arr[i];
+
+        int mayor = 0;
+        for (int i=1; i<n; i++) mayor = max(mayor, arr[i]);
+
+        cout<<mayor+arr[n]<<'\n';
+    }
+
+    return 0;
+}
+
+/*
+
+
+
+ */
