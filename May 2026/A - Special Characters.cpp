57de905a8c12f065e4ac0743652445b429b3850c
Brandom Galder Hernandez Franco <galderhernandez571@gmail.com>
2026-05-29 23:55:19 -0600

    Day 5

---
+++ May 2026/A - Special Characters.cpp
@@ -0,0 +1,50 @@
+//URL: https://codeforces.com/contest/1948/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int MAXN = 200001;
+const int MOD = 1e9 + 7;
+#define pb push_back
+
+#define all(c) (c).begin(), (c).end()
+#define find_set(c, a) ((c).find(a) != (c).end())
+#define in_range(x, l, r) ((x) >= (l) && (x) <= (r))
+#define debug(x) cout << #x << " -> " << x <<'\n';
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while (t--) {
+        int n;
+        cin>>n;
+
+        if (n&1) cout<<"NO\n";
+        else {
+            cout<<"YES\n";
+            char letra = 'A';
+            for (int i=1; i<=n/2; i++) {
+                cout<<letra<<letra;
+                if (letra == 'Z') letra = 'A';
+                else letra++;
+            }
+            cout<<'\n';
+        }
+    }
+
+    return 0;
+}
+
+/*
+
+
+
+ */
